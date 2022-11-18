4b5fadb98dd52b20dc97742aa31e1cd96c929fa8
gzuzmark <alonso.m144@.com>
2022-11-17 22:55:28 -0500

    feat: Add staging test and modify raffle deploy file to point to goerli

--- apps/hardhat-ts/hardhat.config.ts
+++ apps/hardhat-ts/hardhat.config.ts
@@ -1,99 +1,142 @@
-import "dotenv/config";
-import { HardhatUserConfig } from "hardhat/types";
-import "hardhat-deploy";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-gas-reporter";
-import "@typechain/hardhat";
+import "dotenv/config";
 import "solidity-coverage";
-import { node_url, accounts, addForkConfiguration } from "./utils/network";
+import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL ||
+  "https://eth-rinkeby.alchemyapi.io/v2/your-api-key";
+const KOVAN_RPC_URL =
+  process.env.KOVAN_RPC_URL ||
+  "https://eth-kovan.alchemyapi.io/v2/your-api-key";
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL ||
+  "https://polygon-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic";
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const POLYGONSCAN_API_KEY =
+  process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
+const REPORT_GAS = process.env.REPORT_GAS || false;
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-goerli.g.alchemy.com/v2/aCDA3UyoE4Ta0AsY5Y9rQhDYbiI2cX9j";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // }
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //accounts: {
+      //     mnemonic: MNEMONIC,
+      // },
+      saveDeployments: true,
+      chainId: 42,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 5,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 4,
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      saveDeployments: true,
+      chainId: 1,
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 137,
+    },
+  },
+  etherscan: {
+    // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+      kovan: ETHERSCAN_API_KEY,
+      polygon: POLYGONSCAN_API_KEY,
+      goerli: ETHERSCAN_API_KEY,
+    },
+    customChains: [],
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+      default: 1,
+    },
+  },
   solidity: {
     compilers: [
       {
         version: "0.8.9",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000,
-          },
-        },
+      },
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.4.24",
       },
     ],
   },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    player: {
-      default: 1,
-    }
-  },
-  networks: addForkConfiguration({
-    hardhat: {
-      initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
-      chainId: 31337,
-    },
-    localhost: {
-      url: node_url("localhost"),
-      accounts: accounts(),
-    },
-    staging: {
-      url: node_url("goerli"),
-      accounts: accounts("goerli"),
-    },
-    production: {
-      url: node_url("mainnet"),
-      accounts: accounts("mainnet"),
-    },
-    mainnet: {
-      url: node_url("mainnet"),
-      accounts: accounts("mainnet"),
-    },
-    rinkeby: {
-      url: node_url("rinkeby"),
-      accounts: accounts("rinkeby"),
-    },
-    kovan: {
-      url: node_url("kovan"),
-      accounts: accounts("kovan"),
-    },
-    goerli: {
-      url: node_url("goerli"),
-      accounts: accounts("goerli"),
-      blockConfirmations: 6,
-    },
-  }),
-
-  paths: {
-    sources: "src",
-  },
-  gasReporter: {
-    currency: "USD",
-    gasPrice: 100,
-    outputFile: 'gas-report.txt',
-    enabled: process.env.REPORT_GAS ? true : false,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-    maxMethodDiff: 10,
-  },
-  typechain: {
-    outDir: "../web/src/typechain",
-    target: "ethers-v5",
-  },
   mocha: {
-    timeout: 200000,
+    timeout: 500000, // 200 seconds max for running tests
   },
-  external: process.env.HARDHAT_FORK
-    ? {
-        deployments: {
-          // process.env.HARDHAT_FORK will specify the network that the fork is made from.
-          // these lines allow it to fetch the deployments from the network being forked from both for node and deploy task
-          hardhat: ["deployments/" + process.env.HARDHAT_FORK],
-          localhost: ["deployments/" + process.env.HARDHAT_FORK],
-        },
-      }
-    : undefined,
 };
 
 export default config;
--- apps/hardhat-ts/helper-hardhat-config.ts
+++ apps/hardhat-ts/helper-hardhat-config.ts
@@ -37,7 +37,8 @@ export const networkConfig: networkConfigInfo = {
   },
   5: {
     name: "goerli",
-    subscriptionId: "6926",
+    // subscriptionId: "6926",
+    subscriptionId: "6634",
     gasLane:
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
     keepersUpdateInterval: "30",
